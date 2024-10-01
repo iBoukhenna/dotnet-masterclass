ada737da8290e97838b05cbebb741255b7dddef1
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2024-10-01 17:19:00 -0400

    add console app with serilog using docker

---
+++ CarvedRock.InvoiceGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+using Serilog.Events;
+
+var name = typeof(Program).Assembly.GetName().Name;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithProperty("Assembly", name)
+    .WriteTo.Seq("http://host.docker.internal:5341")
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting host");
+    Console.WriteLine("Hello, World!");
+    Log.Information("Finished execution!");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+
+
+
