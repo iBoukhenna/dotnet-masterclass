4f8bb0a29c5392183d08b5a3ed3aea05c8364846
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2024-10-06 20:30:49 -0400

    04-clean-class-and-methods

--- carvedrock.bl/Program.cs
+++ carvedrock.bl/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using carvedrock.bl.Conventions.LanguageGuidelines;
 
 Console.WriteLine("Hello, World!");
 
-var andor = new AndOr();
