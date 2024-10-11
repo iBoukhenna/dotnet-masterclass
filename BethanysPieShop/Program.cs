194dec9cb5d957d0ac72e070b686f301df3dae06
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2024-10-11 15:02:10 -0400

    adding routes and navigation

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -3,26 +3,35 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+//builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 
-builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
     options.UseSqlServer(
-        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]
-    );
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
 
 var app = builder.Build();
 
-app.UseStaticFiles();
-
+//app.MapGet("/", () => "Hello World!");
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
-app.MapDefaultControllerRoute();
+app.UseStaticFiles();
+
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 DbInitializer.Seed(app);
 
