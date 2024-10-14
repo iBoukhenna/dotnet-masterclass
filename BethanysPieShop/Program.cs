c91759a5455f14349295ca66d71566c05d31e96f
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2024-10-14 00:56:59 -0400

    creating form using model binding and validation

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
 
 builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
 builder.Services.AddSession(); 
